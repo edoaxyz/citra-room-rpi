2f27f92527eee2b6350aa7dd45a293897b853c9b
Edoardo Grassi <edo.g@hotmail.it>
2018-08-23 18:38:07 +0200

    Added lib folder

---
+++ lib/gcc/arm-linux-gnueabihf/8.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-8.1.0/configure -v --enable-languages=c,c++ --prefix=/usr/local/gcc-8.1.0 --program-suffix=-8.1.0 --with-arch=armv6 --with-fpu=vfp --with-float=hard --build=arm-linux-gnueabihf --host=arm-linux-gnueabihf --target=arm-linux-gnueabihf";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10e" }, { "arch", "armv6" }, { "float", "hard" }, { "fpu", "vfp" }, { "tls", "gnu" } };
